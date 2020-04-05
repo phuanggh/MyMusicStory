6a6191cfe159896c2118f29ff8cd3101d2014c13
Penny Huang <penny.huang.pw@outlook.com>
2020-04-05 22:30:11 +0800

    improve struct Song

--- MyMusicStory/Models/SongData.swift
+++ MyMusicStory/Models/SongData.swift
@@ -8,29 +8,29 @@
 
 import Foundation
 
-struct SongType {
+struct Song {
     let name: String
     let artist: String
 }
 
 struct SongData {
     static let songList = [
-        SongType(name: "Secret Love Song", artist: "Little Mix"),
-        SongType(name: "哪裡只得我共你",artist: "Dear Jane"),
-        SongType(name: "推開世界的門", artist: "楊乃文"),
-        SongType(name: "少女的祈禱", artist: ""),
-        SongType(name: "不要離我太遠", artist: ""),
-        SongType(name: "今天清晨", artist: ""),
-        SongType(name: "惡作劇", artist: ""),
-        SongType(name: "怎麼還不愛", artist: ""),
-        SongType(name: "勇氣", artist: ""),
-        SongType(name: "小幸運", artist: ""),
-        SongType(name: "寫不出來", artist: ""),
-        SongType(name: "親愛的你怎麼不在身邊", artist: ""),
-        SongType(name: "經過一些秋與冬", artist: ""),
-        SongType(name: "成全", artist: ""),
-        SongType(name: "A New Day Has Come", artist: ""),
-        SongType(name: "The Last Time", artist: ""),
+        Song(name: "Secret Love Song", artist: "Little Mix"),
+        Song(name: "哪裡只得我共你",artist: "Dear Jane"),
+        Song(name: "推開世界的門", artist: "楊乃文"),
+        Song(name: "少女的祈禱", artist: ""),
+        Song(name: "不要離我太遠", artist: ""),
+        Song(name: "今天清晨", artist: ""),
+        Song(name: "惡作劇", artist: ""),
+        Song(name: "怎麼還不愛", artist: ""),
+        Song(name: "勇氣", artist: ""),
+        Song(name: "小幸運", artist: ""),
+        Song(name: "寫不出來", artist: ""),
+        Song(name: "親愛的你怎麼不在身邊", artist: ""),
+        Song(name: "經過一些秋與冬", artist: ""),
+        Song(name: "成全", artist: ""),
+        Song(name: "A New Day Has Come", artist: ""),
+        Song(name: "The Last Time", artist: ""),
     ]
 }
 
